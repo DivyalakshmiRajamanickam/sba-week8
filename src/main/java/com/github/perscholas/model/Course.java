2ce985543fbcc3e17f90d2c7d7950ba56f2d3689
Divyalakshmi <prakidiv@gmail.com>
2020-02-19 14:58:19 -0500

    2/19/2020 2.58pm

--- src/main/java/com/github/perscholas/model/Course.java
+++ src/main/java/com/github/perscholas/model/Course.java
@@ -1,5 +1,46 @@
 package com.github.perscholas.model;
 
 // TODO - Annotate and Implement respective interface and define behaviors
-public class Course {
+public class Course implements CourseInterface{
+
+    private String id;
+    private String name;
+    private String instructor;
+
+
+    public Course(String id, String name, String instructor) {
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+    }
+
+    @Override
+    public Integer getId() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getInstructor() {
+        return null;
+    }
+
+    @Override
+    public void setId(Integer id) {
+
+    }
+
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public void setInstructor(String instructor) {
+
+    }
 }
---
+++ src/main/resources/students.create-table.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE `student` (
+
+   `email` varchar(20) DEFAULT NULL,
+   `name` varchar(20) DEFAULT NULL,
+   `password` varchar(20) DEFAULT NULL,
+
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SELECT * FROM mysql_database.student;
